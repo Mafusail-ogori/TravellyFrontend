4cb73fe744c730d7347f2c35e33355bdc7f98b96
Mafusaillo <danylo.prots.kn.2021@lpnu.ua>
2023-05-01 00:52:27 +0300

    Body fix

--- src/Body/PictureSwiper.js
+++ src/Body/PictureSwiper.js
@@ -1,17 +1,27 @@
 import{Swiper, SwiperSlide} from 'swiper/react';
-import styles from 'swiper/css';
+import {Navigation, Pagination} from "swiper";
 
 const PictureSwiper = (props) => {
     return<Swiper
-        spaceBetween={50}
-        slidesPerView={3}
-        onSlideChange={() => console.log('slide change')}
-        onSwiper={(swiper) => console.log(swiper)}
+        slidesPerView={1}
+        spaceBetween={30}
+        loop={true}
+        pagination={{
+            clickable: true,
+        }}
+        navigation={true}
+        modules={[Pagination, Navigation]}
+        className="mySwiper"
     >
         <SwiperSlide>Slide 1</SwiperSlide>
         <SwiperSlide>Slide 2</SwiperSlide>
         <SwiperSlide>Slide 3</SwiperSlide>
         <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide>
     </Swiper>
 }
 
