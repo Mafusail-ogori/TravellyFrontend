01f02137e52c2c20ef661f03235b16b266b016fd
Mafusaillo <danylo.prots.kn.2021@lpnu.ua>
2023-05-29 13:39:47 +0300

    small fix

--- src/UI/TripCard.js
+++ src/UI/TripCard.js
@@ -1,8 +1,27 @@
 import styles from './TripCard.module.css'
 
 const TripCard = (props) =>{
-    return<div className={styles.form_card}>
-        {props.children}
+    return <div className={styles.form_card}>
+        <img src = {props.image} alt="Not fond"></img>
+        <div className={styles.info_container}>
+            <div className={styles.options_container}>
+                <h2>{props.title}</h2>
+                <h2>{props.price} <i className={"fa-solid fa-hryvnia-sign"}/></h2>
+            </div>
+            <div className={styles.options_container}>
+                <div>
+                    <p>Звідки: <span>{props.startCountry}</span></p>
+                    <p>Куди: <span>{props.endCountry}</span></p>
+                    <p>Готель: <span>{props.hotel}</span></p>
+                    <p>Трансфер: <span>{props.transfer}</span></p>
+                </div>
+                <div>
+                    <p>Починається: <span>{props.startDate}</span></p>
+                    <p>Закінчується: <span>{props.endDate}</span></p>
+                    <p>Що по їжі: <span>{props.food}</span></p>
+                </div>
+            </div>
+        </div>
     </div>
 }
 
--- src/pages/Company/ReviewTrip/AllTrips.js
+++ src/pages/Company/ReviewTrip/AllTrips.js
@@ -1,18 +1,35 @@
 import ContentWrapper from "../../../UI/ContentWrapper";
 import FormCard from "../../../UI/FormCard";
 import getToken from "../../../util/GetToken";
+import axios from "axios";
+import {useEffect, useState} from "react";
+import TripCard from "../../../UI/TripCard";
 
 const AllTrips = () => {
+    const [allTrips, setAllTrips] = useState([])
     const getAllTrips =  async () => {
-        await axios.get('', {
+        await axios.get('http://localhost:8080/company/review-trip', {
             headers: {
                 Authorization : `Bearer ${getToken()}`
             }
+        }).then((res) => {
+            setAllTrips(res.data)
+        }).catch((e) => {
+            console.log(e)
         })
     }
+    useEffect(() => {
+        getAllTrips()
+    }, [])
+
     return <ContentWrapper whiteText = "ПЕРЕГЛЯДАЙ" blackText = "ВДОСКОНАЛЮЙ" color = "#8194FF" height = "1000px">
         <FormCard>
-            {}
+            {allTrips.map((trip) => (
+                <TripCard image = {trip.trip_image} title = {trip.trip_name} startCountry = {trip.trip_start_country}
+                          endCountry = {trip.trip_destination_country} hotel = {trip.trip_hotel}
+                transfer = {trip.trip_transfer} startDate = {trip.trip_start_date} endDate = {trip.trip_end_date} food = {trip.trip_food}
+                price = {trip.trip_price}/>
+            ))}
         </FormCard>
     </ContentWrapper>
 }
